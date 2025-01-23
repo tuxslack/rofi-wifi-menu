9119621c2ee55d056506bff49fc23874ba181a4e
Fernando <33039864+tuxslack@users.noreply.github.com>
2025-01-23 19:59:58 -0300

    Add files via upload

--- rofi-wifi-menu.sh
+++ rofi-wifi-menu.sh
@@ -12,7 +12,16 @@
 # Data da atualização:  22/01/2025 as 19:38:37
 #
 # Licença:  GPL - https://www.gnu.org/
-
+#
+#
+# Você provavelmente vai querer colocar o script no seu $PATH para poder executá-lo como 
+# um comando e mapear uma combinação de teclas para ele.
+#
+# echo $PATH
+#
+# mv -i  rofi-wifi-menu.sh /usr/local/bin/
+#
+# chmod +x  /usr/local/bin/rofi-wifi-menu.sh
 
 
 # https://www.youtube.com/watch?v=v8w1i3wAKiw
@@ -36,6 +45,16 @@ GREEN='\e[1;32m'
 YELLOW='\e[1;33m'
 NC='\e[0m' # sem cor
 
+
+# ----------------------------------------------------------------------------------------
+
+# Carregar arquivos .mo com textdomain
+
+# Define a variável de ambiente do idioma
+
+export LANG="$LANG"
+export TEXTDOMAIN=rofi-wifi-menu
+export TEXTDOMAINDIR=/usr/share/locale
 
 # ----------------------------------------------------------------------------------------
 
@@ -114,18 +133,8 @@ fi
 
 
 
-# ----------------------------------------------------------------------------------------
-
-# Carregar arquivos .mo com textdomain
-
-# Define a variável de ambiente do idioma
-
-export LANG="$LANG"
-export TEXTDOMAIN=rofi-wifi-menu
-export TEXTDOMAINDIR=/usr/share/locale
 
 # ----------------------------------------------------------------------------------------
-
 
 # Função para verificar se um comando está instalado.
 
@@ -143,16 +152,240 @@ check_command() {
 
 # Verificando dependências.
 
-for cmd in "notify-send" "nmcli" "sed" "rofi" "dmenu" "fc-list" "gettext"; do
+for cmd in "notify-send" "nmcli" "sed" "rofi" "dmenu" "fc-list" "gettext" "yad" "sudo" "sed" "grep"; do
 
     check_command "$cmd"
 
 done
 
+# ----------------------------------------------------------------------------------------
+
+
+# Solução de problemas
+#
+# O PopOS! não tem a biblioteca notify-send instalada por padrão. Você pode instalá-la 
+# com o seguinte comando (de acordo com este tópico):
+#
+# https://unix.stackexchange.com/questions/685247/what-is-the-notify-send-alternative-command-in-pop-os
+#
+#
+# sudo apt update
+#
+# sudo apt install -y libnotify-bin
+
+
+# Verifica se o arquivo /etc/os-release existe
+
+if [ ! -f /etc/os-release ]; then
+
+    # Exibe uma tela de erro usando yad.
+
+    yad --center --title="$(gettext 'Error')" --text="$(gettext 'The /etc/os-release file was not found! 
+This may indicate that the system is not Linux or the system structure is corrupt.')" --button="OK":0 --width="300" --height="150"
+
+    exit 1
+
+# else
+
+#    echo -e "\n${GREEN}$(gettext 'The file /etc/os-release exists.') ${NC}\n"
+
+fi
+
+
+# ----------------------------------------------------------------------------------------
+
 
 # O problema é que no NixOS você precisa instalar o pacote libnotify junto com o dunst.
+#
+# A documentação não inclui uma biblioteca que está faltando na versão do PopOS baseada 
+# no Ubuntu!
+#
+#
+# NixOS tem o arquivo /etc/os-release. Ele pode ser usado para identificar o sistema 
+# operacional, mas o conteúdo dele pode ser um pouco diferente de outras distribuições, 
+# porque o NixOS é uma distribuição única e independente.
 
-# A documentação não inclui uma biblioteca que está faltando na versão do PopOS baseada no Ubuntu!
+if grep -q -i "nixos" /etc/os-release  ; then
+
+
+# NixOS usa o Nix como seu gerenciador de pacotes e o sistema é configurado de maneira 
+# declarativa. Para instalar pacotes, você deve adicionar o pacote à configuração do 
+# sistema.
+
+# Caminho para o arquivo de configuração
+
+CONFIG_FILE="/etc/nixos/configuration.nix"
+
+# Verificar se libnotify-bin já está na configuração
+
+if grep -q "libnotify-bin" "$CONFIG_FILE"; then
+
+    echo -e "\n${GREEN}$(gettext 'The libnotify-bin package is already in the configuration.') ${NC}\n"
+        
+else
+
+# O NixOS utiliza um arquivo de configuração centralizado chamado configuration.nix, onde 
+# você especifica todos os pacotes e configurações do sistema. Para instalar libnotify-bin, 
+# você precisa modificar esse arquivo.
+
+    # Adicionar o pacote libnotify-bin à configuração
+
+    echo -e "\n${GREEN}$(gettext 'Adding libnotify-bin to configuration...') ${NC}\n"
+
+
+    # Adicione libnotify-bin à seção environment.systemPackages:
+
+    sed -i '/environment\.systemPackages\s*=/a \\tlibnotify-bin' "$CONFIG_FILE"
+    
+
+# Exemplo:
+# 
+# environment.systemPackages = with pkgs; [
+#   libnotify-bin
+# ];
+
+
+    # Aplicar a configuração com nixos-rebuild
+
+    # Isso vai reconstruir o sistema e instalar o pacote libnotify-bin junto com outras 
+    # alterações na configuração.
+
+    echo -e "\n${GREEN}$(gettext 'Rebuilding the system to apply the changes...') ${NC}\n"
+
+    sudo nixos-rebuild switch
+    
+
+    # Verificar se a instalação foi bem-sucedida
+
+    if which notify-send > /dev/null 2>&1; then
+
+        echo -e "\n${GREEN}$(gettext 'libnotify-bin was installed successfully.') ${NC}\n"
+
+    else
+
+        echo -e "\n${RED}$(gettext 'There was an error installing libnotify-bin.') ${NC}\n"
+
+    fi
+fi
+
+
+
+# else
+
+#      echo -e "\n${RED}$(gettext 'This part of the script is intended for NixOS or derivatives of it.') ${NC}\n"
+
+fi
+
+# ----------------------------------------------------------------------------------------
+
+
+
+# Verificar se o sistema é PopOS! ou um derivado do Debian
+
+if grep -q -i "pop" /etc/os-release || grep -q -i "debian" /etc/os-release || grep -q -i "ubuntu" /etc/os-release || grep -q -i "linuxmint" /etc/os-release || grep -q -i "kali" /etc/os-release || grep -q -i "zorin" /etc/os-release || grep -q -i "elementary" /etc/os-release || grep -q -i "bodhi" /etc/os-release || grep -q -i "peppermint" /etc/os-release || grep -q -i "deepin" /etc/os-release ; then
+
+
+    # Verificar se a biblioteca libnotify-bin está instalada
+
+    if ! dpkg -l | grep -q "libnotify-bin"; then
+
+        # Usar yad para solicitar a instalação
+
+        yad --center --title="$(gettext 'Installation required')" --text="$(gettext 'The libnotify-bin library is not installed. Do you want to install it?')" --button="$(gettext 'Install')":0 --button="$(gettext 'Cancel')":1
+
+        if [ $? -eq 0 ]; then
+
+            # Instalar a biblioteca
+
+            
+            # Executa o comando sudo apt update && sudo apt install -y libnotify-bin
+
+            sudo apt update && sudo apt install -y libnotify-bin
+
+            # Verifica se o comando anterior teve erro
+
+              if [ $? -ne 0 ]; then
+
+                       echo -e "\n${RED}$(gettext 'Command failed! An error occurred while trying to install libnotify-bin.') ${NC}\n"
+
+                  else
+
+                       echo -e "\n${GREEN}$(gettext 'Command executed successfully. The library has been installed.') ${NC}\n"
+              fi
+
+        fi
+
+    else
+
+        echo -e "\n${GREEN}$(gettext 'The libnotify-bin library is already installed.') ${NC}\n"
+         
+    fi
+
+# else
+
+#    echo -e "\n${RED}$(gettext 'This part of the script is intended for PopOS! or Debian-derived systems.') ${NC}\n"
+
+fi
+
+    
+
+
+# ----------------------------------------------------------------------------------------
+
+# Para verificar se a pasta /usr/share/icons/Adwaita/ existe no sistema.
+
+
+# A pasta /usr/share/icons/Adwaita contém os ícones do tema Adwaita, que é o tema de 
+# ícones padrão do GNOME. Se essa pasta não existir, significa que o tema de ícones Adwaita 
+# não está instalado corretamente no sistema, ou o diretório foi removido.
+# 
+# Para garantir que o tema de ícones Adwaita esteja instalado corretamente, você pode 
+# verificar se ele está instalado e, caso contrário, notificar o usuário para 
+# instalar os pacotes necessários.
+
+
+
+
+# Caminho da pasta
+
+DIR="/usr/share/icons/Adwaita"
+
+
+# Verifica se a pasta existe
+
+if [ ! -d "$DIR" ]; then
+
+    # Exibe uma notificação caso a pasta não exista.
+    
+    # Sugestão para o usuário instalar o pacote.
+
+
+        message="$(gettext "The %s folder does not exist. This means that the Adwaita theme icons are not installed. 
+
+Suggestion: 
+
+To fix this, install the 'adwaita-icon-theme' package to get the default icons.")"
+
+
+
+    yad --center --title="$(gettext 'Error')" --text="$(printf "$message" "$DIR")" --button="OK":0 --width="300" --height="150"
+
+
+    # Opcional: Também poderia ser feito a instalação automática se o pacote estiver ausente
+    # para instalar automaticamente em sistemas baseados em Debian/Ubuntu:
+    #
+    # sudo apt update && sudo apt install -y adwaita-icon-theme
+
+    exit 1
+
+fi
+
+
+   # Caso a pasta exista.
+
+#   message="$(gettext 'The %s folder exists and the Adwaita icon theme is installed.')"
+
+#   echo -e "\n${GREEN}$(printf "$message" "$DIR") ${NC}\n"
 
 
 # ----------------------------------------------------------------------------------------
@@ -234,7 +467,7 @@ if   [[ "$wifi_status" =~ "enabled" || "$wifi_status" =~ "habilitado" ]]; then
 
 # Exibe uma notificação informando que a busca de redes Wi-Fi está em andamento.
 
-notify-send "$(gettext 'Getting list of available Wi-Fi networks...')"
+notify-send -i "/usr/share/icons/Adwaita/symbolic/status/network-wireless-connected-symbolic.svg" "$(gettext 'Getting list of available Wi-Fi networks...')"
 
 
 # Aguarda 1 segundo para exibir a notificação
@@ -248,6 +481,7 @@ sleep 1
 
 
 # nmcli device wifi list
+
 
 # Original
 
@@ -270,10 +504,6 @@ elif [[ "$wifi_status" =~ "disabled" || "$wifi_status" =~ "desabilitado" ]]; the
 	    toggle="󰖩  $(gettext 'Enable Wi-Fi')"
 
 
-
-
-
-# fi do "Habilitar Wi-Fi"
 
 fi
 
@@ -372,7 +602,7 @@ else
         message=$(gettext 'Failed to connect to %s. Please check the network.')
 
 
-        nmcli connection up id "$chosen_id" | grep "successfully" && notify-send "$(gettext 'Connection Established')" "$success_message" || notify-send -i "/usr/share/icons/Adwaita/symbolic/status/network-wireless-offline-symbolic.svg" "$(gettext 'Wi-Fi Connection Error')" "$(printf "$message" "'$chosen_id'")"
+        nmcli connection up id "$chosen_id" | grep "successfully" && notify-send  -i "/usr/share/icons/Adwaita/symbolic/status/network-wireless-connected-symbolic.svg" "$(gettext 'Connection Established')" "$success_message" || notify-send -i "/usr/share/icons/Adwaita/symbolic/status/network-wireless-offline-symbolic.svg" "$(gettext 'Wi-Fi Connection Error')" "$(printf "$message" "'$chosen_id'")"
 
     else
 
@@ -389,7 +619,7 @@ else
         message=$(gettext 'Failed to connect to %s. Please check the network.')
 
 
-        nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send "$(gettext 'Connection Established')" "$success_message" || notify-send -i "/usr/share/icons/Adwaita/symbolic/status/network-wireless-offline-symbolic.svg" "$(gettext 'Wi-Fi Connection Error')" "$(printf "$message" "'$chosen_id'")"
+        nmcli device wifi connect "$chosen_id" password "$wifi_password" | grep "successfully" && notify-send -i "/usr/share/icons/Adwaita/symbolic/status/network-wireless-connected-symbolic.svg" "$(gettext 'Connection Established')" "$success_message" || notify-send -i "/usr/share/icons/Adwaita/symbolic/status/network-wireless-offline-symbolic.svg" "$(gettext 'Wi-Fi Connection Error')" "$(printf "$message" "'$chosen_id'")"
 
 
 
